819a4e3a2ffd55adffac70f01447ffcbd3d21fa9
Mariela Aguirre <marielaaguirre610@gmail.com>
2026-06-17 23:46:51 -0300

    nuevo boton de mostrar mas

--- src/components/ui/Buttons/ButtonTextVisible.tsx
+++ src/components/ui/Buttons/ButtonTextVisible.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { jsx } from "react/jsx-runtime";
 
 interface TextoCortoProps {
     texto: string;
@@ -10,35 +9,23 @@ function TextoCorto({ texto, limite = 100 }: TextoCortoProps) {
     const [mostrarTodo, setMostrarTodo] = useState(false);
     const esTextoLargo = texto.length > limite;
 
-    const textoAMostrar = mostrarTodo
-        ? texto
-        : texto.substring(0, limite) + "...";
-
     return (
-        <div style={{maxWidth: "400px", lineHeight: "1.5" }}>
-            <p>
-                {esTextoLargo ? textoAMostrar : texto}
-            </p>
+        <div className="max-w-md">
+            <div
+                className={`overflow-hidden transition-all duration-500 ease-in-out ${mostrarTodo ? "max-h-125" : "max-h-18"
+                    }`} >
+                <p> {texto} </p>
+            </div>
             {esTextoLargo && (
                 <button
                     onClick={() => setMostrarTodo(!mostrarTodo)}
-                    style={style} 
+                    className="mt-4 text-sm font-satoshi text-white bg-gray-200/30 hover:bg-gray-100/50  transition-colors duration-200 ease-in-out px-2 py-1 rounded-full hover:cursor-pointer"
                 >
-                    {mostrarTodo ? "Ver menos" : "Ver más"}
+                    {mostrarTodo ? "Ver menos ↑" : "Ver más ↓"}
                 </button>
             )}
         </div>
     );
 }
 
-const style = {
-    marginTop: "5px",
-    background: "#5E5E5E",
-    border: "0px solid grey",
-    padding: "2px 8px",
-    borderRadius: "15px",
-    color: "white",
-    cursor: "pointer",
-    fontWeight: "normal",
-};
 export default TextoCorto;
